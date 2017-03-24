1b8438c66e35ea9f28424b587d05f4045a725162
Karmanno <zenbudd@mail.ru>
2017-03-24 17:03:10 +1000

    Removed blank lines

--- volcanoes_input/archive.py
+++ volcanoes_input/archive.py
@@ -6,11 +6,9 @@ class AcrhiveGetter:
         self.cam_name = 'SHV1'
         self.years = ['2015', '2016']
 
-
     @staticmethod
     def form_months(self):
         return [i for i in range(1, 13)]
-
 
     @staticmethod
     def form_days(self, year, month):
